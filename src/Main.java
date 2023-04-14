6eab128610633c4ed1c479601ce403347738c8df
ashensam <iamashen27@gmail.com>
2023-04-14 16:23:18 +0530

    creating object and call the method to get output

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Newclass ashen=new Newclass();
+        ashen.stId();
+    }
+}
---
+++ src/Newclass.java
@@ -0,0 +1,13 @@
+import javax.xml.transform.stream.StreamSource;
+
+public class Newclass {
+
+    String name="ashen dilranga samarasekera ";
+    int age=21;
+
+    public void stId(){
+
+        System.out.println("\n My name is: "+name+"\n"+" My age is : "+age);
+    }
+
+}
