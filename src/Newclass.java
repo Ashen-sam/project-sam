65555c96991bc37f949a4bd6e75e733648fc4115
ashensam <iamashen27@gmail.com>
2023-04-14 17:39:07 +0530

    second edit
    Co-Authored-By: ashen-sam2 <130757359+ashen-sam2@users.noreply.github.com>

--- src/Newclass.java
+++ src/Newclass.java
@@ -2,13 +2,13 @@ import javax.xml.transform.stream.StreamSource;
 
 public class Newclass {
 
-    String name="ashen dilranga samarasekera ";
-    int age=21;
+    String name = "ashen dilranga samarasekera ";
+    int age = 21;
 
-    public void stId(){
+    public void stId() {
 
-        System.out.println("\n My name is: "+name+"\n"+" My age is : "+age);
-        System.out.println("ashensammmmm");
+        System.out.println("\n My name is: " + name + "\n" + " My age is : " + age);
+        System.out.println("ashen dilranga samarasekera and i'm a going to push the code ");
     }
 
 }
